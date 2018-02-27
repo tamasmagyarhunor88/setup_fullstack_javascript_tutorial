8bc1c0baa8b5222f8efeacd984da1ca42ada53d9
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-27 12:02:27 +0000

    Fix deprecated syntax

--- postgres-express-react-node-tutorial/app.js
+++ postgres-express-react-node-tutorial/app.js
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.urlencoded( { extended: false } ));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format
 app.get('*', (req, res) => res.status(200).send({
